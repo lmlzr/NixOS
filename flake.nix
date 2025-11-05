a99c42683d71c843c0044e969fb9bc9b2eb9bf12
Leon Melzer <viruskillertv@gmail.com>
2025-11-05 19:32:58 +0100

    First flake commit

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "Main flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+	nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+		modules = [ ./configuration.nix ];
+	};
+
+  };
+}
