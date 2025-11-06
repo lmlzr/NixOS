0b993e4c53fd44c68f6b1b97fcb0e76b5c65c23b
lmlzr <viruskillertv@gmail.com>
2025-11-06 17:37:02 +0100

    changes

---
+++ modules/de/kde.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, ... }:
+{
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "de";
+    variant = "";
+  };
+  }
