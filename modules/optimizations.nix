398f1f57469d1daa38d4cdff6bdc7d12bc8db53c
lmlzr <viruskillertv@gmail.com>
2025-11-09 19:22:37 +0100

    faster boot time

--- modules/optimizations.nix
+++ modules/optimizations.nix
@@ -1,6 +1,8 @@
 { config, pkgs, lib, inputs, ... }:
 
 {
+    kernelParams = [ "qiet" ];
+    loader.timeout = 1;
     #schneller builden
     nix.settings.max-jobs = "auto";
     nix.settings.cores = 0; 
