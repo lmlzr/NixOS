3ea3130cb58d3485e70b5efa3a7dbd9f9903011f
lmlzr <viruskillertv@gmail.com>
2025-11-09 19:25:21 +0100

    faster boot time

--- modules/optimizations.nix
+++ modules/optimizations.nix
@@ -1,8 +1,9 @@
 { config, pkgs, lib, inputs, ... }:
 
 {
-    kernelParams = [ "qiet" ];
-    loader.timeout = 1;
+    systemd.services.networkManager-wait-online.wantedBy = lib.mkForce [];
+    boot.kernelParams = [ "qiet" ];
+    boot.loader.timeout = 1;
     #schneller builden
     nix.settings.max-jobs = "auto";
     nix.settings.cores = 0; 
