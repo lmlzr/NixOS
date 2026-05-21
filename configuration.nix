08f1fbda7e11a91d34d26022d019c1ea97a92f7d
lmlzr <viruskillertv@gmail.com>
2026-05-21 18:15:31 +0200

    switching to amd

--- configuration.nix
+++ configuration.nix
@@ -12,7 +12,7 @@
       ./modules/system/desktop.nix
       ./modules/hardware/optimizations.nix
       ./modules/de/kde.nix
-      ./modules/hardware/nvidia.nix
+      #./modules/hardware/nvidia.nix
       ./modules/wireless/bluetooth.nix
       ./modules/boot/systemdboot.nix
     ];
